39dd25c80edb1f0f40d59d92e77a5ac811d347f5
Fleur Broca <brocafleur@gmail.com>
2014-11-11 14:46:36 +0100

    task CRUD model view controller

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,24 +1,5 @@
 class Task < ActiveRecord::Base
 
-  def index         # GET /tasks
-  end
 
-  def show          # GET /tasks/:id
-  end
-
-  def new           # GET /tasks/new
-  end
-
-  def create        # POST /tasks
-  end
-
-  def edit          # GET /tasks/:id/edit
-  end
-
-  def update        # PATCH /tasks/:id
-  end
-
-  def destroy       # DELETE /tasks/:id
-  end
 
 end
